90a328dec5f9205e3b3632d0d16718d7318d0066
Stefania Tsvetkova <nia_flo@abv.bg>
2020-11-25 11:25:03 +0200

    Added file functionality (open, read, close)

--- bankingSoftware.cpp
+++ bankingSoftware.cpp
@@ -1,5 +1,64 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#define FILE_NAME "users.txt"
+#define LINE_DELIMITER ':'
+
+bool openFile (std::fstream& file) {
+    file.open (FILE_NAME, std::ios::in | std::ios::out);
+
+    if (!file.is_open()) {
+        return false;
+    }
+
+    return true;
+}
+
+std::vector<std::string> splitLine(std::string line, char delimiter) {
+    std::vector<std::string> result;
+
+    for (int i = 0; i < line.size(); ++i) {
+        std::string current = "";
+
+        for (; i < line.size() && line[i] != delimiter; ++i) {
+            current += line[i];
+        }
+
+        result.push_back(current);
+    }
+
+    return result;
+}
+
+void readFile (std::fstream& file, std::vector<std::string> &usernames, std::vector<std::string> &passwords, std::vector<double> &balances) {
+    std::string line;
+
+    while (std::getline(file, line)) {
+        std::vector<std::string> parameters = splitLine(line, LINE_DELIMITER);
+
+        usernames.push_back(parameters[0]);
+        passwords.push_back(parameters[1]);
+        balances.push_back(std::stod(parameters[2]));
+    }
+}
 
 int main () {
+    std::fstream usersFile;
+
+    if (!openFile (usersFile)) {
+        std::cout << "Error - database file could not be loaded.";
+
+        return 0;
+    }
+
+    std::vector<std::string> usernames, passwords;
+    std::vector<double> balances;
+
+    readFile (usersFile, usernames, passwords, balances);
+
+    usersFile.close();
+
     return 0;
 }
