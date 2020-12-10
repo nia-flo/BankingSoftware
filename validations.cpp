b1daf1e4f08a4cb8db3a9bd58be7b9d1888c65bd
Stefania Tsvetkova <nia_flo@abv.bg>
2020-12-10 16:01:04 +0200

    Moved all validations to a separate file.

---
+++ validations.cpp
@@ -0,0 +1,112 @@
+#include "user.h"
+
+#include <fstream>
+#include <string>
+#include <vector>
+
+#define MAX_OVERDRAFT -10000
+#define POSSIBLE_PASSWORD_SYMBOLS "!@#$%^&*"
+#define POSSIBLE_USERNAME_SYMBOLS "_-."
+#define MIN_PASSWORD_LENGHT 5
+
+bool isFileEmpty (std::fstream& file) {
+    return file.peek() == std::ifstream::traits_type::eof();
+}
+
+bool isDouble (std::string text) {
+    bool isDotUsed = false;
+
+    for (int i = 0; i < text.size(); ++i) {
+        if (text[i] == '.') {
+            if (!isDotUsed && i != text.size() - 1) {
+                isDotUsed = true;
+            } else {
+                return false;
+            }
+        }
+        else if (text[i] == '-') {
+            if (i != 0) {
+                return false;
+            }
+        }
+        else if (text[i] < '0' || text[i] > '9') {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool isPossibleSymbol(char symbol, std::string possibleSymbols) {
+    for (int i = 0; i < possibleSymbols.size(); ++i) {
+        if (symbol == possibleSymbols[i]) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool isUsernameValid(std::string username) {
+    for (int i = 0; i < username.size(); ++i) {
+        if (!((username[i] >= 'a' && username[i] <= 'z') || (username[i] >= 'A' && username[i] <= 'Z') || isPossibleSymbol(username[i], POSSIBLE_USERNAME_SYMBOLS))) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool isUsernameTaken (std::string username, std::vector<user> &users) {
+    for (int i = 0; i < users.size(); ++i) {
+        if (username == users[i].username) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+//returns:
+//1 if the password is valid
+//-1 if the password's lenght is less than 5 characters
+//-2 is the password contains characters that are not allowed
+//-3 if the password doesnt contain a lowercase letter
+//-4 if the password doesnt contain an uppercase letter
+//-5 if the password doesnt contain a symbol
+int isPasswordValid (std::string password) {
+    if (password.size() < MIN_PASSWORD_LENGHT) {
+        return -1;
+    }
+
+    bool containsLowercaseLetter = false, containsUppercaseLetter = false, containsSymbol = false;
+
+    for (int i = 0; i < password.size(); ++i) {
+        if (password[i] >= 'a' && password[i] <= 'z') {
+            containsLowercaseLetter = true;
+        }
+        else if (password[i] >= 'A' && password[i] <= 'Z') {
+            containsUppercaseLetter = true;
+        }
+        else if (isPossibleSymbol(password[i], POSSIBLE_PASSWORD_SYMBOLS)) {
+            containsSymbol = true;
+        }
+        else if (password[i] < '0' || password[i] > '9') {
+            return -2;
+        }
+    }
+
+    if (!containsLowercaseLetter) {
+        return -3;
+    }
+
+    if (!containsUppercaseLetter) {
+        return -4;
+    }
+
+    if (!containsSymbol) {
+        return -5;
+    }
+
+    return 1;
+}
